9f2ff2af79d0eb1ea24d90ba4979d413fc25bafe
brad morgan <bkmorgan3@gmail.com>
2017-08-07 09:39:24 -0700

    connect to mongo, add middleware

--- index.js
+++ index.js
@@ -2,11 +2,51 @@ const
   express = require('express'),
   app = express()
   yelp = require('yelp-fusion'),
-  dotenv = require('dotenv').load()
-  PORT = 3000,
-  token = process.env.TOKEN
+  dotenv = require('dotenv').load(),
+  mongoose = require('mongoose'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  jwt = require('jsonwebtoken'),
+  cors = require('cors'),
+  PORT = process.env.PORT || 3000,
+  token = process.env.TOKEN,
+  client = yelp.client(token)
 
 
+//=============Connect to Mongo======
+const mongoUrl = (process.env.MONGO_URL || 'mongodb://localhost/tenderDB')
+mongoose.connect(mongoUrl, (err) => {
+  console.log(err || 'connected to MongoDB');
+});
+
+
+// ==========Setup Middleware
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+
+// =================API===============
+  client.search({
+    term :'restaurants',
+    location: 'los angeles, ca'
+  }).then(response => {
+    // console.log(response.jsonBody)
+  }).catch(e => {
+    // console.log(e)
+  });
+  // client.business('electric-owl-los-angeles').then(response => {
+  //   console.log(response.jsonBody.photos);
+  // }).catch(e => {
+  //   console.log(e);
+  // });
+
+//  ==========ROUTES========
+
+
+app.get('/', (req,res) =>{
+  res.json({message: 'heellllllo'})
+})
 
 app.listen(PORT, () => {
   console.log(`server is litening on port ${PORT}`)
